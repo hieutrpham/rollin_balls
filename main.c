471ad20425c1e35dd120bb005a1f90aa5cb65631
Hieu Pham <hieutrungpham91@gmail.com>
2025-09-27 17:36:49 +0300

    feat: animate the ball with ansi escape sequence

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "algebra.h"
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define WIDTH 64
 #define HEIGHT 64
@@ -21,17 +22,22 @@ void fill(t_pixel c)
 		*ptr++ = c;
 }
 
+/* @brief: compressed the ascii characters by combining 2 rows into 1 row
+ */
 void show(void)
 {
 	static char row[WIDTH];
-	static char *table = " _^C";
+	static char table[PIXEL_N][PIXEL_N] = {
+		{' ', '_'},
+		{'^', '$'},
+	};
 	for (int y = 0; y < HEIGHT/2; y++)
 	{
 		for (int x = 0; x < WIDTH; x++)
 		{
 			t_pixel top = display[2*y*WIDTH + x];
 			t_pixel bot = display[(2*y+1)*WIDTH + x];
-			row[x] = table[(top<<1)|bot];
+			row[x] = table[top][bot];
 		}
 		fwrite(row, WIDTH, 1, stdout);
 		fputc('\n', stdout);
@@ -61,23 +67,31 @@ void circle(t_v2i center, int radius)
 		{
 			t_v2i diff = v2i_sub(center, v2i_build(x, y));
 			if (v2i_sqlen(diff) < r*r)
-				display[y*WIDTH + x] = '*';
+				display[y*WIDTH + x] = FORE;
 		}
 	}
+}
+
+#define FPS 2
+void back(void)
+{
+	printf("\x1b[%dD", WIDTH);
+	printf("\x1b[%dA", HEIGHT/2);
 }
 
 int main(void)
 {
 	t_v2i pos = v2i_build(WIDTH/2, HEIGHT/2);
 	t_v2i velocity = v2i_build(0, 5);
-
-	for (int i = 0; i < 10; i++)
+	
+	while (1)
 	{
 		pos = v2i_sum(pos, velocity);
 		fill(BACK);
 		circle(pos, 13);
 		show();
-		printf("-------------------------------------------------------------------\n");
+		back();
+		usleep(1000*1000/FPS);
 	}
 	return 0;
 }
